67106ebc719e38c54e756d2bef96176342a8a71b
jb <jbbbregore099@gmail.com>
2023-05-23 05:29:55 +0800

    prep cattle feed>record

--- app/Domains/Herds/Validators/FeedRecordCreateValidator.ts
+++ app/Domains/Herds/Validators/FeedRecordCreateValidator.ts
@@ -1,28 +1,26 @@
-import { schema, rules, CustomMessages } from '@ioc:Adonis/Core/Validator'
-import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import { schema, rules, CustomMessages } from "@ioc:Adonis/Core/Validator";
+import type { HttpContextContract } from "@ioc:Adonis/Core/HttpContext";
 
 export default class FeedRecordCreateValidator {
   constructor(protected ctx: HttpContextContract) {}
 
   public schema = schema.create({
     feedNameId: schema.number([
-      rules.exists({ table: 'feed_names', column: 'name' })
+      rules.exists({ table: "feed_names", column: "name" }),
     ]),
     date: schema.date(),
-    quantity: schema.number([ rules.range(1, 10000000000000) ]),
-    consumedBy: schema.enum(['individual', 'group'] as const),
-    consumer: schema.string({ trim: true }, [
-      rules.minLength(1),
-    ]),
-    notes: schema.string.optional({ trim: true }, [
-      rules.minLength(5),
-    ])
-  })
+    consumedBy: schema.enum(["individual", "group"] as const),
+    quantity: schema.number([rules.range(1, 10000000000000)]),
+    consumer: schema.string({ trim: true }, [rules.minLength(1)]),
+    notes: schema.string.optional({ trim: true }, [rules.minLength(5)]),
+  });
 
   public messages: CustomMessages = {
-    required: '{{ field }} is required',
-    minLength: '{{ field }} must be at least {{ options.minLength }} characters long',
-    range: '{{ field }} must be between {{ options.start }} and {{ options.stop }}',
-    'enum': '{{ field }} must be any of the following values: {{ options.choices }}',
-  }
+    required: "{{ field }} is required",
+    minLength:
+      "{{ field }} must be at least {{ options.minLength }} characters long",
+    range:
+      "{{ field }} must be between {{ options.start }} and {{ options.stop }}",
+    enum: "{{ field }} must be any of the following values: {{ options.choices }}",
+  };
 }

2669626b2c20ce7668039dac072afb51cf7325ad
jb <jbbbregore099@gmail.com>
2023-05-22 20:30:38 +0800

    fix minor

--- app/Domains/Users/Validators/UserCreateValidator.ts
+++ app/Domains/Users/Validators/UserCreateValidator.ts
@@ -1,20 +1,27 @@
-import { schema, rules, CustomMessages } from '@ioc:Adonis/Core/Validator'
-import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import { schema, rules, CustomMessages } from "@ioc:Adonis/Core/Validator";
+import type { HttpContextContract } from "@ioc:Adonis/Core/HttpContext";
 
 export default class UserCreateValidator {
   constructor(protected ctx: HttpContextContract) {}
 
   public schema = schema.create({
-    username: schema.string({}, [ rules.minLength(5) ]),
-    firstName: schema.string({}, [ rules.minLength(2) ]),
-    lastName: schema.string({}, [ rules.minLength(2) ]),
-    email: schema.string({}, [ rules.email(), rules.unique({ table: 'users', column: 'email' }) ]),
-    password: schema.string({}, [ rules.minLength(2) ]),
-  })
+    firstName: schema.string({}, [rules.minLength(2)]),
+    lastName: schema.string({}, [rules.minLength(2)]),
+    email: schema.string({}, [
+      rules.email(),
+      rules.unique({ table: "users", column: "email" }),
+    ]),
+    username: schema.string({}, [
+      rules.minLength(5),
+      rules.unique({ table: "users", column: "username" }),
+    ]),
+    password: schema.string({}, [rules.minLength(2)]),
+  });
 
   public messages: CustomMessages = {
-    required: '{{ field }} is required',
-    minLength: '{{ field }} must be at least {{ options.minLength }} characters',
-    unique: '{{ field }} already exists',
-  }
+    required: "{{ field }} is required",
+    minLength:
+      "{{ field }} must be at least {{ options.minLength }} characters",
+    unique: "{{ field }} already exists",
+  };
 }

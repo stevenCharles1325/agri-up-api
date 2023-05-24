a0dc7cf52b05fc163cefde4aaff381de1fc72e5e
jb <jbbbregore099@gmail.com>
2023-05-25 06:37:01 +0800

    add view for income/expense page

--- app/Routes/Expenses/index.ts
+++ app/Routes/Expenses/index.ts
@@ -1,12 +1,12 @@
-import Route from '@ioc:Adonis/Core/Route'
+import Route from "@ioc:Adonis/Core/Route";
 
 Route.group(() => {
-  Route.get('/', 'ExpensesController.index')
-  Route.get('/:expenseId', 'ExpensesController.show')
+  Route.get("/", "ExpensesController.index");
+  Route.get("/:expenseId", "ExpensesController.show");
 
-  Route.post('/', 'ExpensesController.store')
-  Route.put('/:expenseId', 'ExpensesController.update')
-  Route.delete('/:expenseId', 'ExpensesController.destroy')
+  Route.post("/", "ExpensesController.store");
+  Route.put("/:expenseId", "ExpensesController.update");
+  Route.delete("/:expenseId", "ExpensesController.destroy");
 })
-.prefix('/expenses')
-.namespace('App/Domains/Expenses/Controllers')
+  .prefix("/expenses")
+  .namespace("App/Domains/Expenses/Controllers");

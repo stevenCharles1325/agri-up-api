3c43e54ef12f329bd0528d5135f3a32ddbf7d99d
stevenCharles1325 <stevencharles1325@gmail.com>
2023-05-29 20:45:02 +0800

    fix: deleted_at issue
    Signed-off-by: stevenCharles1325 <stevencharles1325@gmail.com>

--- database/migrations/1684584428014_milk_reductions.ts
+++ database/migrations/1684584428014_milk_reductions.ts
@@ -24,7 +24,7 @@ export default class extends BaseSchema {
        */
       table.timestamp("created_at", { useTz: true }).defaultTo(this.now());
       table.timestamp("updated_at", { useTz: true }).defaultTo(this.now());
-      table.timestamp("deleted_at", { useTz: true }).defaultTo(null);
+      table.timestamp("deleted_at", { useTz: true }).nullable().defaultTo(null);
     });
   }
 
